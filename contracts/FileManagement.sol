ca9bc0f2d688cbb44521d76222e3a61fce1dc6c8
phangjunyu <phangjunyu@gmail.com>
2019-08-30 15:44:26 +0800

    latest remixed

--- contracts/FileManagement.sol
+++ contracts/FileManagement.sol
@@ -12,27 +12,28 @@ contract FileManagement {
         Permissions permission;
         uint expiryTime; //0 means permission persists infinitely
     }
+    
     mapping (address => Permission) filePermissions;
-    string[] history;
+    address[] updaterHistory;
     address[] users;
+    string[] locationHistory;
     
     struct File {
         uint fileHash;
-        uint version;
-        string location;
+        uint currentVersion;
         address[] users;
-        string[] history;
+        address[] updaterHistory;
+        string[] locationHistory;
         mapping (address=>Permission) permissions;
         mapping (address=>uint) permissionsExpiry;
     }
     
     File file;
-    File[] files;
     
     event NewFile(address indexed _owner, uint indexed _fileHash);
-    event ReadPermissionGranted(address indexed _user, uint indexed _fileHash, uint duration);
-    event WritePermissionGranted(address indexed _user, uint indexed _fileHash, uint duration);
-    event OwnerPermissionGranted(address indexed _user, uint indexed _fileHash, uint duration);
+    event ReadPermissionGranted(address indexed _user, uint indexed _fileHash, uint expiryTime);
+    event WritePermissionGranted(address indexed _user, uint indexed _fileHash, uint expiryTime);
+    event OwnerPermissionGranted(address indexed _user, uint indexed _fileHash, uint expiryTime);
     event FileUpdated(address indexed _user, uint indexed _fileHash, uint new_fileHash);
 
     modifier isOwner(uint _fileHash){
@@ -65,14 +66,17 @@ contract FileManagement {
         mapping(address => Permission) storage _filePermissions = filePermissions;
         _filePermissions[msg.sender].permission = Permissions.Owner;
         _filePermissions[msg.sender].expiryTime = 0;
-        //initialize history array
-        string[] storage _history = history;
-        //initialize users array and add in owner
+        
+        //initialize updaterHistory, locationHistory and users array
+        address[] storage _updaterHistory = updaterHistory;
+        _updaterHistory.push(msg.sender);
+        string[] storage _locationHistory = locationHistory;
+        _locationHistory.push(_location);
         address[] storage _users = users;
         users.push(msg.sender);
         
         // created in memory
-        file = File(_fileHash,0,_location,_users,_history);
+        file = File(_fileHash,0, _users,_updaterHistory, _locationHistory);
         file.permissions[msg.sender] = Permission(Permissions.Owner, 0);
         fileDirectory[_fileHash] = file;
         usersToFiles[msg.sender].push(_fileHash);
@@ -119,16 +123,15 @@ contract FileManagement {
     }
 
     function updateFile(uint _fileHash, uint new_fileHash, string calldata new_location) external isWriter(_fileHash) {
-        //update version and add old location into history
-        fileDirectory[_fileHash].version++;
-        fileDirectory[_fileHash].history.push(fileDirectory[_fileHash].location);
-        //updates location and new fileHash
-        fileDirectory[_fileHash].location = new_location;
+        //increment currentVersion, update locationHistory and updaterHistory
+        fileDirectory[_fileHash].currentVersion++;
+        fileDirectory[_fileHash].locationHistory.push(new_location);
+        fileDirectory[_fileHash].updaterHistory.push(msg.sender);
+        //updates new fileHash
         fileDirectory[_fileHash].fileHash = new_fileHash;
         //set new_fileHash to map to the file in fileDirectory
         fileDirectory[new_fileHash] = fileDirectory[_fileHash];
 
-        
         for (uint i = 0; i < fileDirectory[_fileHash].users.length; i++){
             for (uint j = 0; j < usersToFiles[fileDirectory[_fileHash].users[i]].length; j++){
                 if (usersToFiles[fileDirectory[_fileHash].users[i]][j] == _fileHash){
@@ -137,7 +140,6 @@ contract FileManagement {
                     break;
                 }
             }
-            
         }
 
         delete fileDirectory[_fileHash];
@@ -150,8 +152,28 @@ contract FileManagement {
     function viewFilePermissionsForUser(uint _fileHash, address _user) external view isReader(_fileHash) returns (Permissions){
         return fileDirectory[_fileHash].permissions[_user].permission;
     }
-
+    
+    // strings are an array of bytes so Solidity cannot return array of array. can use Seriality library in future implementation
+    // function viewLocationHistory(uint _fileHash) external view isReader(_fileHash) returns (string[] memory){
+    //     return fileDirectory[_fileHash].locationHistory;
+    // }
+    
+    function viewUpdaterHistory(uint _fileHash) external view isReader(_fileHash) returns (address[] memory){
+        return fileDirectory[_fileHash].updaterHistory;
+    }
+    
     function verifyDocument(uint _fileHash) external view isReader(_fileHash){
 
+    }
+    
+    function viewUpdater(uint _fileHash) internal view isReader(_fileHash) returns (address){
+        return (fileDirectory[_fileHash].updaterHistory[fileDirectory[_fileHash].currentVersion]);
+    }
+    
+    function viewLocation(uint _fileHash) internal view isReader(_fileHash) returns (string memory){
+        return fileDirectory[_fileHash].locationHistory[fileDirectory[_fileHash].currentVersion];
+    }
+    function viewFile(uint _fileHash) external view isReader(_fileHash) returns (uint, string memory, address){
+        return (fileDirectory[_fileHash].currentVersion, viewLocation(_fileHash), viewUpdater(_fileHash));
     }
 }
